11b75ccf65bb73141bb54aedbc64e8561491090e
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2023-02-01 05:48:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-7xl text-center font-bold bg-purple-600 text-white p-12">
+        আসতেসি খুবই তাড়াতাড়ি...
+      </h2>
+    </div>
+  );
+}
+
+export default App;
